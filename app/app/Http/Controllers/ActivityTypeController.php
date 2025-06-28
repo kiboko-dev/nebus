5e700b66be88b80d33eef00f86603cdadfaff1c6
Konstantin Kaznacheev <kazna.k.rad@yandex.ru>
2025-06-28 10:56:09 +0300

    Добавлены пакеты для описания API и DTO, описаны служебные методы

--- app/app/Http/Controllers/ActivityTypeController.php
+++ app/app/Http/Controllers/ActivityTypeController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers;
 use App\Http\Repositories\ActivityTypeRepository;
 use App\Models\ActivityType;
 use Illuminate\Http\JsonResponse;
+use Knuckles\Scribe\Attributes\Endpoint;
+use Knuckles\Scribe\Attributes\Group;
 
+#[Group(name: 'Виды деятельности')]
 class ActivityTypeController extends Controller
 {
+    #[Endpoint(title: 'Получить список')]
     public function index(): JsonResponse
     {
         return response()->json(app(ActivityTypeRepository::class)->index());
--- app/app/Http/Controllers/BuildingController.php
+++ app/app/Http/Controllers/BuildingController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Repositories\BuildingRepository;
 use Illuminate\Http\JsonResponse;
+use Knuckles\Scribe\Attributes\Endpoint;
+use Knuckles\Scribe\Attributes\Group;
 
+#[Group(name: 'Здания')]
 class BuildingController extends Controller
 {
+    #[Endpoint(title: 'Получить список')]
     public function index(): JsonResponse
     {
         return response()->json(app(BuildingRepository::class)->index());
